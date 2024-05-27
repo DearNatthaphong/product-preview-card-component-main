9a6423e1815ea32a1f032d8775d7990443c5fe21
LAPTOP-F25A12VH\Dear <natthaphong.ea@gmail.com>
2024-05-28 02:18:34 +0700

    Mobile-first solution using tailwindcss and css flexbox

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primaryDarkCyan: 'hsl(158, 36%, 37%)',
+        primaryCream: 'hsl(30, 38%, 92%)',
+        neutralVeryDarkBlue: 'hsl(212, 21%, 14%)',
+        neutralDarkGrayishBlue: 'hsl(228, 12%, 48%)',
+        neutralWhite: 'hsl(0, 0%, 100%)',
+        neutralVeryDarkCyan: 'hsl(158, 43%, 18%)'
+      },
+
+      fontFamily: {
+        montserrat: ['Montserrat'],
+        fraunces: ['Fraunces']
+      }
+    }
+  }
+};
